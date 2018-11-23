66094e30b6691b7bcdaf70721da3c8c06956c62c
Daniel Rivas <ers.daniel+dev@gmail.com>
2018-11-22 18:17:55 -0600

    Exported library

--- dist/typing.js
+++ dist/typing.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -302,4 +312,5 @@ window.Typing = Typing;
 
 /***/ })
 /******/ ]);
+});
 //# sourceMappingURL=typing.js.map
